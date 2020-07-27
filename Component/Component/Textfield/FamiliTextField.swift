65616ef017509ec6c39d65188e078240873c532c
William William <william.inxcorp@gmail.com>
2020-07-27 16:46:46 +0700

    add documentation for familiTextField

--- Component/Component/Textfield/FamiliTextField.swift
+++ Component/Component/Textfield/FamiliTextField.swift
@@ -8,35 +8,58 @@
 
 import UIKit
 
+/** Famili Custom Text Field
+ 
+ Create textfield with custom attributes and design
+ How to use:
+ * Using XIB
+    * Drag and drop a UIView and change the class using CircularProgressView
+ * Programatically
+    * Init using the custom initializer to set all the required value
+ */
 @IBDesignable public class FamiliTextField: UITextField {
     
-    @IBInspectable var borderColor: UIColor? {
-        didSet {
-            self.layer.borderColor = borderColor?.cgColor
-        }
-    }
-    
+    // MARK: - Property
+    /// State of the current textfield
+    ///
+    /// Available state :
+    /// * Normal
+    /// * Focused
+    /// * Error
     public enum FamiliTextFieldState {
         case normal
         case focused
         case error
     }
     
+    /// Border color for the textfield
+    var borderColor: UIColor? {
+        didSet {
+            self.layer.borderColor = borderColor?.cgColor
+        }
+    }
+    
+    // MARK: - Initialization
+    /// Default init
     public override init(frame: CGRect) {
         super.init(frame: frame)
         setupTextField()
     }
     
+    /// Default init
     required init?(coder: NSCoder) {
         super.init(coder: coder)
         setupTextField()
     }
     
+    /// Update view for IB usage
     public override func prepareForInterfaceBuilder() {
         super.prepareForInterfaceBuilder()
         self.setupTextField()
     }
     
+    // MARK: - Function
+    /// Setup the textfield default attributes
     private func setupTextField() {
         self.delegate = self
         self.borderColor = UIColor(hex: FamiliTextFieldConstant.BorderColor.normal.rawValue)
@@ -49,6 +72,7 @@ import UIKit
         self.layer.masksToBounds = true
     }
     
+    /// Animate the border color shifting
     private func animate(with duration: TimeInterval, _ color: String) {
         let borderAnimation = CABasicAnimation(keyPath: "borderColor")
         borderAnimation.fromValue = self.borderColor?.cgColor
@@ -58,19 +82,25 @@ import UIKit
         borderAnimation.timingFunction = CAMediaTimingFunction(name: .easeInEaseOut)
         self.layer.borderColor = UIColor(hex: color)?.cgColor
         self.layer.add(borderAnimation, forKey: "borderChangeColor")
-        
     }
     
+    /// Provide insets for the text
     public override func textRect(forBounds bounds: CGRect) -> CGRect {
         return bounds.insetBy(dx: CGFloat(FamiliTextFieldConstant.CommonValue.textLeading),
                               dy: 0)
     }
     
+    /// Provide insets for the text
     public override func editingRect(forBounds bounds: CGRect) -> CGRect {
         return bounds.insetBy(dx: CGFloat(FamiliTextFieldConstant.CommonValue.textLeading),
                               dy: 0)
     }
     
+    /// Set current state of the textfield
+    /// Based on FamiliTextFieldState, change the border color with animation
+    /// - parameters:
+    ///     - state: Available states are **Normal**,  **Focused**, **Error**
+    ///
     public func setState(state: FamiliTextFieldState) {
         switch state {
         case .normal:
