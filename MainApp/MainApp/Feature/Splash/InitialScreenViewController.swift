0d9b0e0a7654503b7e68244d9de5bec31dc7ae67
Evan Christian <evanchristian98@gmail.com>
2020-07-27 14:15:51 +0700

    Initial Screen View

---
+++ MainApp/MainApp/Feature/Splash/InitialScreenViewController.swift
@@ -0,0 +1,33 @@
+//
+//  InitialScreenViewController.swift
+//  MainApp
+//
+//  Created by Evan Christian on 27/07/20.
+//  Copyright © 2020 William Inx. All rights reserved.
+//
+
+import UIKit
+
+class InitialScreenViewController: UIViewController {
+
+    @IBOutlet weak var initialScreenImageView: UIImageView!
+    @IBOutlet weak var initialScreenLabel: UILabel!
+    @IBOutlet weak var initialScreenArrowButton: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
