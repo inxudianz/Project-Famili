4c391be1c3764f08411d7181f06f297e06aa584e
William William <william.inxcorp@gmail.com>
2020-07-30 16:27:10 +0700

    Add authentication handler and reroute info plist location

---
+++ MainApp/MainApp/Resources/Handler/FirebaseHandler.swift
@@ -0,0 +1,28 @@
+//
+//  FirebaseHandler.swift
+//  MainApp
+//
+//  Created by William Inx on 30/07/20.
+//  Copyright © 2020 William Inx. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+class FirebaseHandler {
+    class AuthenticationHandler {
+        public static func signUp(email: String, password: String, completion: @escaping AuthDataResultCallback) {
+            Auth.auth().createUser(withEmail: email, password: password) { (result, error) in
+                completion(result, error)
+            }
+        }
+        
+        public static func signIn(email: String, password: String, completion: @escaping AuthDataResultCallback) {
+            Auth.auth().signIn(withEmail: email, password: password) { (result, error) in
+                completion(result, error)
+            }
+        }
+    }
+    
+    
+}
