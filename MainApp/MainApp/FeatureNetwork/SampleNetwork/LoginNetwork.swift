15a21508bae07603bc54a16aeb4ff11b490b1d3c
Evan <evanchristian98@gmail.com>
2020-08-06 18:25:09 +0700

    LoginModel LoginResponse LoginPath LoginSubPath LoginService

---
+++ MainApp/MainApp/FeatureNetwork/SampleNetwork/LoginNetwork.swift
@@ -0,0 +1,9 @@
+//
+//  LoginNetwork.swift
+//  MainApp
+//
+//  Created by Evan Christian on 06/08/20.
+//  Copyright © 2020 William Inx. All rights reserved.
+//
+
+import Foundation
---
+++ MainApp/MainApp/Network/Path/FeaturePath/Sample/LoginSubPath.swift
@@ -0,0 +1,13 @@
+//
+//  LoginSubPath.swift
+//  MainApp
+//
+//  Created by Evan Christian on 06/08/20.
+//  Copyright © 2020 William Inx. All rights reserved.
+//
+
+import Foundation
+
+enum LoginSubPath: String {
+    case login = "login"
+}
