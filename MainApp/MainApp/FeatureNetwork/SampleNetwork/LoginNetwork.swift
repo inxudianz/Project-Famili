bea70f7bcea6db24cc7ecd01ae24e0a8c22a5e24
Evan <evanchristian98@gmail.com>
2020-08-06 18:40:47 +0700

    LoginNetwork

--- MainApp/MainApp/FeatureNetwork/SampleNetwork/LoginNetwork.swift
+++ MainApp/MainApp/FeatureNetwork/SampleNetwork/LoginNetwork.swift
@@ -7,3 +7,39 @@
 //
 
 import Foundation
+import RxSwift
+
+protocol LoginNetworkDelegate: class {
+    func didSuccessLogin(response: LoginResponse)
+    func didFailedLogin(error: Error)
+}
+
+protocol LoginNetworkProtocol {
+    var loginNetworkDelegate: LoginNetworkDelegate? { get set }
+    
+    func loginPost()
+}
+
+class LoginNetwork: LoginNetworkProtocol{
+    var loginNetworkDelegate: LoginNetworkDelegate?
+    
+    private var networkService: NetworkService
+    
+    init() {
+        self.networkService = NetworkService()
+    }
+    
+    func loginPost() {
+        let loginModelLoginInfo = LoginModel.LoginInfo(email: "asdasd@gmail.com", password: "11113333")
+        networkService.request(LoginService.loginRequest, loginModelLoginInfo, LoginResponse.self) { [weak self] (result) in
+            switch result {
+            case .success(let response):
+                self?.loginNetworkDelegate?.didSuccessLogin(response: response)
+            case .failure(let error):
+                self?.loginNetworkDelegate?.didFailedLogin(error: error)
+            }
+        }
+    }
+    
+    
+}
