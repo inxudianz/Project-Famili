2daf8bbd061450f0ec07657bbe74dd528e51ff8a
William William <william.inxcorp@gmail.com>
2020-10-21 10:27:52 +0700

    initial orderLanding commit

---
+++ MainApp/MainApp/Coordinator/Feature/Order/OrderCoordinatorProtocol.swift
@@ -0,0 +1,9 @@
+//
+//  OrderCoordinatorProtocol.swift
+//  MainApp
+//
+//  Created by William Inx on 21/10/20.
+//  Copyright © 2020 William Inx. All rights reserved.
+//
+
+import Foundation
---
+++ MainApp/MainApp/Feature/Order/OrderLanding/OrderLandingViewController.swift
@@ -0,0 +1,17 @@
+//
+//  OrderLandingViewController.swift
+//  MainApp
+//
+//  Created by William Inx on 21/10/20.
+//  Copyright © 2020 William Inx. All rights reserved.
+//
+
+import UIKit
+
+class OrderLandingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
