485fe2d4a7535f05e3b34a7152c9374a9b15c5d0
William William <william.inxcorp@gmail.com>
2020-11-17 01:27:57 +0700

    add bubbleChat functionality

---
+++ Component/Component/ChatBubble/FamiliChatBubbleConstant.swift
@@ -0,0 +1,21 @@
+//
+//  FamiliChatBubbleConstant.swift
+//  Component
+//
+//  Created by William Inx on 17/11/20.
+//
+
+import UIKit
+
+internal typealias FCBC = FamiliChatBubbleConstant
+enum FamiliChatBubbleConstant {
+    enum Common {
+        static let defaultMaxWidth:CGFloat = 200
+        static let contentCornerRadius: CGFloat = 16
+        static let textXOffset: CGFloat = 16
+        static let textYOffset: CGFloat = 16
+        static let pointerHeight: CGFloat = 24
+        static let viewWidthPadding: CGFloat = 32
+        static let viewHeightPadding: CGFloat = 32
+    }
+}
